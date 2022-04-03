f34ba4d873a17741fd50c2ec614771cdd4807c06
prisma7771 <prismaputra91@gmail.com>
2022-04-03 14:22:42 +0700

    repair icon position

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,17 @@
             <h2 class="mt-5"><strong>WELCOME TO OUR WEBSITE STORE</strong></h2>
             <hr class="bg-dark border-3 border-top border-dark">
         </div>
-        <div class="d-flex justify-content-around" style="margin-top: 110px;">
+        <div class="d-flex justify-content-around text-center" style="margin-top: 110px;">
             <div>
-                <i class=" fa-solid fa-clipboard-list fa-9x" style="color:black; margin-left:17px;"></i>
+                <i class=" fa-solid fa-clipboard-list" style="color:black; font-size:9vw"></i>
                 <div>
                     <hr>
                     <p style="display: inline-block">Browse Our Product</p>
                 </div>
             </div>
             <div>
-                <i class=" fa-brands fa-whatsapp fa-9x" style="color:green; margin-left:20px;"></i>
-                <div style="margin-right: 18px">
+                <i class=" fa-brands fa-whatsapp" style="color:green; font-size: 9vw"></i>
+                <div>
                     <hr>
                     <p style="display: inline-block;">Contact Our DIstributor</p>
                 </div>
